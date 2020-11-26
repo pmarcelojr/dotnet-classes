f6cb5b60e9dbd3c55026074b0d2f31120fb8918e
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-11-26 19:09:14 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
