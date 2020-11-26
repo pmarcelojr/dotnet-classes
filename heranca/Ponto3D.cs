493e34cc96faf4a225c51ea1d3a334b5b50fc628
pmarcelojr <marcelosantostecnologia@gmail.com>
2020-11-26 19:34:46 -0300

    creating classe Ponto3D.cs

---
+++ heranca/Ponto3D.cs
@@ -0,0 +1,20 @@
+namespace Classes.Heranca
+{
+    public class Ponto3D : Ponto
+    {
+        public int z;
+        public Ponto3D(int x, int y, int z) : base(x, y)
+        {
+            this.z = z;
+            CalcularDistancia();
+        }
+        public static void Calcular()
+        {
+
+        }
+        public override void CalcularDistanciaTwo()
+        {
+            
+        }
+    }
+}
